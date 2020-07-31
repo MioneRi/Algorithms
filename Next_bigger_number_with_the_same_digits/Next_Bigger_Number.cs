df076daac11e724d3f14d45e4790da6e3f09d2e8
MioneRi <thebestguy1101001@gmail.com>
2020-07-31 19:48:12 +0300

    now it works

--- Next_bigger_number_with_the_same_digits/Next_Bigger_Number.cs
+++ Next_bigger_number_with_the_same_digits/Next_Bigger_Number.cs
@@ -23,55 +23,71 @@ namespace SomeTasks
         }
 
         public static long NextBiggerNumber(long n)
-        {
-            long answer;
-            string answerStr = "";
+        {            
+            long answer = -1;            
+            long fact = Fact(n.ToString().Length);
 
             // Converts number to list of chars, it's convenient way to create their possible combinations.
-            List<string> originalDigits = (from x in n.ToString()
-                                           select x.ToString()).ToList();
+            List<string> originalDigits = (from x in n.ToString() 
+                                   select x.ToString()).ToList();            
             // Store all combinations like letters.
             List<string> combinationsStr = new List<string>();
 
             // Store all combinations like numbers of digits.
-            List<string> combinationsInt = new List<string>();
+            List<string> combinationsInt = new List<string>();            
 
             List<string> tmpList = new List<string>();
             for (int i = 0; i < originalDigits.Count; i++)
                 tmpList.Add(i.ToString());
             string tmpStr = "";
 
+            // 1. Fill "combinationsInt".
             func(originalDigits.Count, combinationsInt, tmpList, ref tmpStr);
 
             // 2. Converting "combinationsInt" to "combinationsStr" 
-            //     according to positions of digits in "originalDigits".
-            // 3. Sorting and find bigger number in "combinationsStr".
+            //    according to positions of digits in "originalDigits".
+            long lengthOfCombination = originalDigits.Count();
+            for (int i = 0; i < fact; i++)
+            {
+                combinationsStr.Add("");
+                for (int j = 0; j < lengthOfCombination; j++)
+                {
+                    combinationsStr[i] += originalDigits[Int32.Parse(combinationsInt[i][j].ToString())];
+                }
+            }
 
-            if (long.TryParse(answerStr, out answer))
-                return answer;
-            else
-                throw new Exception("An Error with converting string to long!!!\n");
-        }
+            // 3. Converting "combinationsStr" to "combinationsLong" and sorting values.
+            List<long> combinationsLong = (from k in combinationsStr                                           
+                                           select long.Parse(k)).OrderBy(t => t).ToList();
+
+            for (int i = 0; i < fact; i++)
+            {
+                if (combinationsLong[i] > n)
+                    answer = combinationsLong[i];
+            }
+            
+            return answer;                    
+        }        
 
         // k - amount of digits, tmpList - stores list of positions.
-        public static void func(int k, List<string> combinationsInt, List<string> tmpList, ref string tmpStr)
+        public static void func (int k, List<string> combinationsInt, List<string> tmpList, ref string tmpStr)
         {
-            if (k == 0)
+            if (k == 0) 
             {
                 combinationsInt.Add(tmpStr);
                 if (combinationsInt.Count == k - 1)
                     return;
-            }
+            }                
 
             for (int i = 0; i < k; i++)
-            {
+            {                
                 tmpStr += tmpList[i];
 
                 var tmpList2 = new List<string>(tmpList);
                 tmpList2.RemoveAt(i);
-                func(k - 1, combinationsInt, tmpList2, ref tmpStr);
+                func(k-1, combinationsInt, tmpList2, ref tmpStr);
 
-                tmpStr = tmpStr.Substring(0, tmpStr.Length - 1);
+                tmpStr = tmpStr.Substring(0, tmpStr.Length-1);
             }
         }
 
@@ -81,31 +97,7 @@ namespace SomeTasks
             for (int i = 2; i <= n; i++)
                 k *= i;
             return k;
-        }
-
-        //public static long ProdTree(long l, long r)
-        //{
-        //    if (l > r)
-        //        return 1;
-        //    if (l == r)
-        //        return l;
-        //    if (r - l == 1)
-        //        return (long)l * r;
-        //    long m = (l + r) / 2;
-        //    return ProdTree(l, m) * ProdTree(m + 1, r);
-        //}
-
-        //public static long FactTree(long n)
-        //{
-        //    if (n < 0)
-        //        return 0;
-        //    if (n == 0)
-        //        return 1;
-        //    if (n == 1 || n == 2)
-        //        return n;
-        //    return ProdTree(2, n);
-        //}
-
+        }        
     }
 
 }
